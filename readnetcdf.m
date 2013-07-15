7dfe3ecf37bdbc92dff5fde7e181fd44eb2f2c40
Kirk North <kirk.w.north@gmail.com>
2013-07-15 15:34:24 -0400

    Style changes to readnetcdf.m

--- readnetcdf.m
+++ readnetcdf.m
@@ -1,10 +1,10 @@
-function [Data,Atts,GAtts] = readnetcdf(inPath,inFile)
+function [Data, Atts, Gatts] = readnetcdf(fname)
 %READNETCDF Read NetCDF variables, variable attributes, and global
 %attributes.
-%   [Data,Atts,GAtts] = READNETCDF(inPath,inFile) reads the NetCDF file 
-%   given by inPath/inFile and stores all variable data in the structure  
-%   Data, all variable attribute information in the structure Atts, and all
-%   global attribute information in the structure GAtts.
+%   [Data, Atts, Gatts] = READNETCDF(fname) reads the NetCDF file fname
+%   and stores all variable data in the structure Data, all variable 
+%	attribute information in the structure Atts, and all global attribute 
+%	information in the structure Gatts.
 %
 %   This function is able to read any NetCDF convention (e.g. CF, NCAR-RAF,
 %   CDC, etc) and does not require the user to make any changes to the 
@@ -27,25 +27,25 @@ function [Data,Atts,GAtts] = readnetcdf(inPath,inFile)
 %   You should have received a copy of the GNU General Public License
 %   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-% Open NetCDF.
-ncId = netcdf.open(strcat(inPath,inFile),'NC_NOWRITE');
-[nDims,nVars,nGlobalAtts,unlimDim] = netcdf.inq(ncId);
+% Open NetCDF
+nc = netcdf.open(fname, 'NC_NOWRITE');
+[nDims, nVars, nGatts, unlimDim] = netcdf.inq(nc);
 
-% Gather NetCDF dimensions.
+% Gather NetCDF dimensions
 for i = 0:nDims-1
-    [dimName,dimLen] = netcdf.inqDim(ncId,i);
-    fprintf('Dimension name and length = %s; %d\n',dimName,dimLen)
+    [dimName, dimLen] = netcdf.inqDim(nc, i);
+    fprintf('Dimension name and length = %s; %d\n', dimName, dimLen)
 end
 
-% Gather NetCDF variables and variable attributes.
+% Gather NetCDF variables and variable attributes
 for i = 0:nVars-1
-    [varName,xType,dimId,nAtts] = netcdf.inqVar(ncId,i);
-    varData = netcdf.getVar(ncId,i);
+    [varName, xType, dimId, nAtts] = netcdf.inqVar(nc, i);
+    varData = netcdf.getVar(nc, i);
     Data.(varName) = varData;
     for j = 0:nAtts-1
-        attName = netcdf.inqAttName(ncId,i,j);
-        attValue = netcdf.getAtt(ncId,i,attName);
-        if (strcmp(attName,'_FillValue'))
+        attName = netcdf.inqAttName(nc, i, j);
+        attValue = netcdf.getAtt(nc, i, attName);
+        if (strcmp(attName, '_FillValue'))
             Atts.(varName).('FillValue_') = attValue;
         else
             Atts.(varName).(attName) = attValue;
@@ -53,12 +53,12 @@ for i = 0:nVars-1
     end
 end
 
-% Gather global attributes.
-for i = 0:nGlobalAtts-1
-    gAttName = netcdf.inqAttName(ncId,netcdf.getConstant('NC_GLOBAL'),i);
-    gAttValue = netcdf.getAtt(ncId,netcdf.getConstant('NC_GLOBAL'),gAttName);
-    GAtts.(regexprep(gAttName,'\s','_')) = gAttValue; % Adds underscores where ever any whitespace exists.
+% Gather global attributes
+for i = 0:nGatts-1
+    gAttName = netcdf.inqAttName(nc, netcdf.getConstant('NC_GLOBAL'), i);
+    gAttValue = netcdf.getAtt(nc, netcdf.getConstant('NC_GLOBAL'), gAttName);
+    Gatts.(regexprep(gAttName, '\s', '_')) = gAttValue; % Adds underscores where ever any whitespace exists
 end
 
-% Close NetCDF.
-netcdf.close(ncId)
+% Close NetCDF
+netcdf.close(nc)
